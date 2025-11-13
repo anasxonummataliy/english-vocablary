7b55ba0ee6d32986e6fdeff3d739bd08636ccc2e
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-11-13 17:30:16 +0500

    fix:last fix in day

--- app/main.py
+++ app/main.py
@@ -9,18 +9,22 @@ from aiogram.filters import Filter, CommandStart
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
-
-ADMIN = os.getenv("ADMIN")
+TOKEN = os.getenv("TOKEN") or ""
+ADMIN = os.getenv("ADMIN") or ""
 
 dp = Dispatcher()
 
-
+print(ADMIN)
 @dp.message(CommandStart)
 async def start_message(message: Message):
-
-    await message.answer(f"Assalomu alaykum {message.from_user.first_name} bizning botimizga hush kelibsiz!") #type:ignore
-
+    if message.from_user.id != int(ADMIN):  # type:ignore
+        print(type((message.from_user.id)))  # type:ignore
+        if message.from_user.last_name is not None: #type:ignore
+            await message.answer(f"Assalomu alaykum {message.from_user.first_name} {message.from_user.last_name} bizning botimizga hush kelibsiz!") #type:ignore
+        else:
+            await message.answer(f"Assalomu alaykum {message.from_user.first_name} bizning botimizga hush kelibsiz!" ) #type:ignore
+    else:
+        await message.answer("Admin")
 async def main() -> None:
     bot = Bot(TOKEN)  # type:ignore
     await dp.start_polling(bot)
