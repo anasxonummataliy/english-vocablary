97e531bccd6815e1c23ff90ca89a0c6f7e55d3d4
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-11-13 16:28:00 +0500

    fix

--- app/main.py
+++ app/main.py
@@ -10,18 +10,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+
 ADMIN = os.getenv("ADMIN")
 
 dp = Dispatcher()
 
+
+@dp.message(CommandStart)
+async def start_message(message: Message):
+
+    await message.answer(f"Assalomu alaykum {message.from_user.first_name} bizning botimizga hush kelibsiz!") #type:ignore
+
 async def main() -> None:
-    bot = Bot(TOKEN) #type:ignore
+    bot = Bot(TOKEN)  # type:ignore
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
-
-
-    
