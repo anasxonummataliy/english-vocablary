68d2746e85fa007398651d234ad74a9213a534a1
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-12-17 15:28:02 +0500

    fix

--- bot/routers/keyboard.py
+++ bot/routers/keyboard.py
@@ -1,4 +1,11 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+    CallbackQuery,
+)
+
 
 async def level_keyboard():
     kb = ReplyKeyboardBuilder()
@@ -10,5 +17,49 @@ async def level_keyboard():
     return kb
 
 
+ALL_UNITS = [f"Unit {i}" for i in range(1, 61)]
+ITEMS_PER_PAGE = 8
 
 
+async def get_page_data(page: int = 0):
+    start = page * ITEMS_PER_PAGE
+    end = start + ITEMS_PER_PAGE
+    page_data = ALL_UNITS[start:end]
+    total_pages = (len(ALL_UNITS) + ITEMS_PER_PAGE - 1) // ITEMS_PER_PAGE
+    return page_data, page, total_pages
+
+
+async def create_units_keyboard(current_page: int, total_pages: int, units: list):
+    keyboard = []
+
+    for i in range(0, len(units), 2):
+        row = []
+        row.append(
+            InlineKeyboardButton(text=units[i], callback_data=f"select_{units[i]}")
+        )
+        if i + 1 < len(units):
+            row.append(
+                InlineKeyboardButton(
+                    text=units[i + 1], callback_data=f"select_{units[i+1]}"
+                )
+            )
+        keyboard.append(row)
+    nav_buttons = []
+    if current_page > 0:
+        nav_buttons.append(
+            InlineKeyboardButton(text="◀️", callback_data=f"page_{current_page-1}")
+        )
+
+    nav_buttons.append(
+        InlineKeyboardButton(
+            text=f"{current_page + 1}/{total_pages}", callback_data="current"
+        )
+    )
+    if current_page < total_pages - 1:
+        nav_buttons.append(
+            InlineKeyboardButton(text="▶️", callback_data=f"page_{current_page+1}")
+        )
+
+    keyboard.append(nav_buttons)
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
--- bot/routers/start.py
+++ bot/routers/start.py
@@ -32,7 +32,7 @@ async def start_handler(message: Message, bot: Bot):
             await session.commit()
 
     await bot.send_chat_action(chat_id=message.from_user.id, action=ChatAction.TYPING)
-    kb = level_keyboard()
+    kb = await level_keyboard()
     await message.answer(
         f"Assalomu alaykum {message.from_user.first_name} bizning botimizga hush kelibsiz! \n 📚 English Vocablary in Use Kitobni qaysi qismidan boshlamoqchisiz?",
         reply_markup=kb.as_markup(resize_keyboard=True),
