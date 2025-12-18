df705f5c303fbe26a0430577cea994010fd3ade3
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-12-18 16:43:51 +0500

    fix

--- bot/admin/channels.py
+++ bot/admin/channels.py
@@ -3,11 +3,31 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
+from sqlalchemy import select
+
+from bot.database.models.channels import Channel
+from bot.database.session import get_async_session_context
+
 
 class AddChannelStates(StatesGroup):
     channel = State()
 
+
 router = Router()
+
+
+@router.message(Command("channels"))
+async def get_channels(message: Message):
+    async with get_async_session_context() as session:
+        stmt = select(Channel)
+        result = await session.execute(stmt)
+        channels = result.scalars().all()
+    if not channels:
+        await message.answer("Kanallar mavjud emas❗️")
+        return
+    await message.answer(channels)
+
+
 @router.message(Command("/add_channel"))
 async def add_channel(message: Message, state: FSMContext):
     await state.set_state(AddChannelStates.channel)
