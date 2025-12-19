1bbaf5229f519155b91324a92adaf006189d2f56
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-12-19 23:36:31 +0500

    fix

--- bot/admin/broadcast.py
+++ bot/admin/broadcast.py
@@ -1,10 +1,13 @@
-
 from aiogram import Router, Bot
 from aiogram.filters import Command
 from aiogram.types import Message
 
+from bot.database.session import get_async_session_context
+
 router = Router()
+
 
 @router.message(Command("broadcast"))
 async def broadcast_handler(message: Message, bot: Bot):
-    await message.answer("broadcast bo'limi")
+    async with get_async_session_context() as session:  
+        pass
