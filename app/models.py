8406ad23bbd75f620449f0756f444292ebb6e342
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-05-11 00:23:06 +0500

    fix

--- app/models.py
+++ app/models.py
@@ -24,7 +24,8 @@ class User(models.Model):
     username = models.CharField(max_length=255, null=True, blank=True)
     is_blocked = models.BooleanField(default=False)
     last_activity = models.DateTimeField(null=True, blank=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f"{self.first_name} ({self.tg_id})"
 
--- bot/main.py
+++ bot/main.py
@@ -18,33 +18,32 @@ from bot.routers.user_commands import user_command
 from bot.database.base import create_db_and_tables
 
 load_dotenv()
-
 TOKEN = os.getenv("TOKEN") or ""
+
+dp = Dispatcher()
+bot = Bot(TOKEN)
+CHANNEL_ID = os.getenv("CHANNEL_ID") or ""
 ADMIN = int(os.getenv("ADMIN"))
 
-bot = Bot(TOKEN)
-dp = Dispatcher()
+
+@dp.startup()
+async def start_bot(bot: Bot):
+    await bot.send_message(ADMIN, "Bot started ✅")
 
 
 async def start_bot() -> None:
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-
     await bot.delete_webhook(drop_pending_updates=True)
-
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    await bot.set_webhook(os.getenv("WEBHOOK_URL") or "")
+    logging.info(f"{await bot.get_webhook_info()}")
     dp.message.middleware(IsJoinChannelMiddleware())
     dp.message.middleware(UserActivityMiddleware())
-
     dp.include_router(middleware_router)
     dp.include_router(admin_router)
     dp.include_router(user_router)
-
     await create_db_and_tables()
-
     await bot.set_my_commands(admin_commands, scope=BotCommandScopeChat(chat_id=ADMIN))
-
     await bot.set_my_commands(user_command, scope=BotCommandScopeAllPrivateChats())
-
-    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
